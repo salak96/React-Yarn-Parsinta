ac4ec44007d9f760f95c4873c0380eeb58ae3405
salak96 <lambangsasangka@gmail.com>
2023-10-29 15:40:36 +0700

    function Components form

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from './components/Form';
-class App extends Component {
-    constructor() {
-        super();
+import React from 'react';
+import Form from './components/FormFunction';
 
-        this.state = {
-            user: [],
-        };
-    }
-    //axios get user
-    getUser = async () => {
-        let response = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-        this.setState({
-            user: response.data,
-        });
-    };
-    //call getUser
-    componentDidMount() {
-        this.getUser();
-    }
-    render() {
-        //panggil state user
-        const { user } = this.state;
-        return (
-            <div className='container d-flex flex-column align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
-                <div className='p-2'>
-                    <h1>Call api jsonplaceholder</h1>
-                    <h1>My name is {user.name}</h1>
-                    <h1>My User name is {user.username}</h1>
-                    <h1>My email is {user.email}</h1>
-                    <Form />
-                </div>
-            </div>
-        );
-    }
+function App() {
+    return (
+            <Form/>
+    )
 }
 
 export default App;
--- src/components/FormFunction.jsx
+++ src/components/FormFunction.jsx
@@ -1,69 +1,51 @@
-import React, { Component } from 'react';
+import { useState } from "react"
 
-class Form extends Component {
-    constructor() {
-        super()
+export default function FormFunction() {
 
-        this.state = {
-            firstName: '',
-            lastName: '',
-            name    :''
-        }
-    }
-    changeHandler = (e)=>{
-        this.setState({
-            [e.target.name] : e.target.value
-        })
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [name, setName] = useState('');
+
+    const submitHandler = (e) => {
+      e.preventDefault();
+       setName(`${firstName} ${lastName}`);
+       setFirstName('');
+       setLastName('');
     }
 
-    submitHandler = (e)=>{
-        e.preventDefault();
-        this.setState({
-            name : `${this.state.firstName} ${this.state.lastName}`
-        })
-        this.setState({
-            firstName : '',
-            lastName : ''
-        })
-    }
-    render() {
-        const { name } = this.state
-        return (
-            <div className='p-5'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-10'>
-                            <div className='card'>
-                                <div className='card-header'>Learn REACT JS</div>
-                                <div className='card-body'>
-                                    <form onSubmit={this.submitHandler}>
-                                        <div className='mb-4'>
-                                            <label htmlFor='firstName' className='form-label'>
-                                                firstName
-                                            </label>
-                                            <input type='text' value={this.state.firstName} onChange={this.changeHandler} name='firstName' id='firstName' className='form-control' />
-                                        </div>
-                                        <div className='mb-4'>
-                                            <label htmlFor='lastName' className='form-label'>
-                                                lastName
-                                            </label>
-                                            <input type='text' value={this.state.lastName} onChange={this.changeHandler} name='lastName' id='lastName' className='form-control' />
-                                        </div>
-                                        <button type='submit' className='btn btn-primary w-100 btn-block'>
-                                            Show
-                                        </button>
-                                    </form>
+    return(
+        <div className='p-5'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-10'>
+                    <div className='card'>
+                        <div className='card-header'>Learn REACT JS</div>
+                        <div className='card-body'>
+                            <form onSubmit={submitHandler}>
+                                <div className='mb-4'>
+                                    <label htmlFor='firstName' className='form-label'>
+                                        firstName
+                                    </label>
+                                    <input type='text' value={firstName} onChange={(e) => setFirstName(e.target.value)} name='firstName' id='firstName' className='form-control' />
                                 </div>
-                                <div className='card-footer'>
-                                    My name is {name ? name : ''}
+                                <div className='mb-4'>
+                                    <label htmlFor='lastName' className='form-label'>
+                                        lastName
+                                    </label>
+                                    <input type='text' value={lastName} onChange={(e) => setLastName(e.target.value)} name='lastName' id='lastName' className='form-control' />
                                 </div>
-                            </div>
+                                <button type='submit' className='btn btn-primary w-100 btn-block'>
+                                    Show
+                                </button>
+                            </form>
+                        </div>
+                        <div className='card-footer'>
+                            My name is {name ? name : ''}
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    </div>
+    )
 }
-
-export default Form;
