bfe4b33b6e1d9809a2cd14014f5fb7e7fc17358b
salak96 <lambangsasangka@gmail.com>
2023-10-29 13:24:34 +0700

    yarn class components

---
+++ README.md
@@ -0,0 +1,22 @@
+# INSTALASI REACT JS Yarn
+
+- Install Vite react yarn
+```bash
+
+yarn create vite
+```
+
+- Instal package json
+```bash
+yarn
+```
+
+- Running React vite
+```bash
+yarn dev
+```
+
+- Install axios
+```bash
+yarn add axios
+```
---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+
+
+class App extends Component{
+    constructor(){
+        super()
+
+        this.state ={
+            user :[]
+        }
+    }
+    //axios get user
+    getUser = async () =>{
+        let response = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+        this.setState({
+            user : response.data
+        })
+    }
+    //call getUser
+    componentDidMount(){
+        this.getUser()
+    }
+    render(){
+        //panggil state user
+        const {user} = this.state
+        return(
+            <div>
+                <h1>My name is {user.name}</h1>
+                <h1>My User name is {user.username}</h1>
+                <h1>My email is {user.email}</h1>
+            </div>
+
+        );
+    }
+}
+
+export default App;
